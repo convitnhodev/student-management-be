2b5ddf0bc88fcd5080f2650f46036c6784af602d
PhiHungTF <phihungtf@gmail.com>
2023-01-07 16:09:07 +0700

    getclass

--- modules/class/classTransport/getClass.go
+++ modules/class/classTransport/getClass.go
@@ -2,32 +2,24 @@ package classTransport
 
 import (
 	"managerstudent/common/customResponse"
-	"managerstudent/common/solveError"
 	"managerstudent/component"
-	"managerstudent/modules/class/classBiz"
 	"managerstudent/modules/class/classStorage"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func GetClass(app component.AppContext) gin.HandlerFunc {
 	return func(c *gin.Context) {
-
-		var id string
-		var ok bool
-
-		id, ok = c.GetQuery("class_id")
-		if !ok {
-			panic(solveError.ErrInvalidRequest(nil))
-		}
+		classId := c.Request.URL.Query().Get("class_id")
 
 		store := classStorage.NewMongoStore(app.GetNewDataMongoDB())
-		biz := classBiz.NewGetClassesBiz(store)
-		data, err := biz.GetClass(c.Request.Context(), id)
+		data, err := store.FindClass(c.Request.Context(), bson.M{"class_id": classId})
 		if err != nil {
 			c.JSON(400, err)
 			return
 		}
+
 		c.JSON(200, customResponse.SimpleSuccessReponse(data))
 	}
 }
